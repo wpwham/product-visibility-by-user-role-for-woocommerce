af794202e66d8e02476a23c4a844d28297a93ef5
algoritmika <info@algoritmika.com>
2019-02-08 19:13:02 +0200

    v1.4.0

--- includes/alg-wc-pvbur-functions.php
+++ includes/alg-wc-pvbur-functions.php
@@ -2,7 +2,7 @@
 /**
  * Product Visibility by User Role for WooCommerce - Functions
  *
- * @version 1.2.5
+ * @version 1.4.0
  * @since   1.1.0
  * @author  Algoritmika Ltd.
  */
@@ -252,3 +252,91 @@ if ( ! function_exists( 'alg_wc_pvbur_get_current_user_all_roles' ) ) {
 		return ( ! empty( $current_user->roles ) ) ? $current_user->roles : array( 'guest' );
 	}
 }
+
+if ( ! function_exists( 'alg_wc_pvbur_get_invisible_product_terms' ) ) {
+
+	/**
+	 * Gets invisible product terms
+	 *
+	 * @version 1.2.5
+	 * @since   1.2.0
+	 *
+	 * @param string $taxonomy
+	 * @param string $role
+	 *
+	 * @return array
+	 */
+	function alg_wc_pvbur_get_invisible_product_terms( $taxonomy = 'product_cat', $role = 'current_role' ) {
+
+		if ( $role == 'current_role' ) {
+			$user_roles = alg_wc_pvbur_get_current_user_all_roles();
+		} else {
+			if ( is_string( $role ) ) {
+				$user_roles = array( $role );
+			} else {
+				$user_roles = $role;
+			}
+		}
+
+		$invisible_all = array();
+		foreach ( $user_roles as $role ) {
+			if ( $taxonomy == 'product_cat' ) {
+				$invisible = get_option( "alg_wc_pvbur_bulk_invisible_product_cats_{$role}", array() );
+				$invisible = empty( $invisible ) ? array() : $invisible;
+			} else {
+				$invisible = get_option( "alg_wc_pvbur_bulk_invisible_product_tags_{$role}", array() );
+				$invisible = empty( $invisible ) ? array() : $invisible;
+			}
+			if ( ! empty( $invisible ) ) {
+				$invisible_all = array_merge( $invisible_all, $invisible );
+			}
+		}
+		$invisible_all = array_unique( $invisible_all );
+
+		return $invisible_all;
+	}
+}
+
+if ( ! function_exists( 'alg_wc_pvbur_get_visible_product_terms' ) ) {
+
+	/**
+	 * Gets visible product terms
+	 * @version 1.2.5
+	 * @since   1.2.0
+	 *
+	 * @param string $taxonomy
+	 * @param string $role
+	 *
+	 * @return array
+	 */
+	function alg_wc_pvbur_get_visible_product_terms( $taxonomy = 'product_cat', $role = 'current_role' ) {
+
+		if ( $role == 'current_role' ) {
+			$user_roles = alg_wc_pvbur_get_current_user_all_roles();
+		} else {
+			if ( is_string( $role ) ) {
+				$user_roles = array( $role );
+			} else {
+				$user_roles = $role;
+			}
+		}
+
+		$visible_all = array();
+		foreach ( $user_roles as $role ) {
+			if ( $taxonomy == 'product_cat' ) {
+				$visible = get_option( "alg_wc_pvbur_bulk_visible_product_cats_{$role}", array() );
+				$visible = empty( $visible ) ? array() : $visible;
+			} else {
+				$visible = get_option( "alg_wc_pvbur_bulk_visible_product_tags_{$role}", array() );
+				$visible = empty( $visible ) ? array() : $visible;
+			}
+			if ( ! empty( $visible ) ) {
+				$visible_all = array_merge( $visible_all, $visible );
+			}
+		}
+		$visible_all = array_unique( $visible_all );
+
+		return $visible_all;
+	}
+}
+
