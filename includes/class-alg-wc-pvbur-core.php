40d26854354b47129aa74f68d2a2e8da1b40f330
WP Wham <support@wpwham.com>
2020-05-12 18:07:43 -0700

    fix issue where pagination does not work correctly when the WC store page is set as the same page as the home page

--- includes/class-alg-wc-pvbur-core.php
+++ includes/class-alg-wc-pvbur-core.php
@@ -85,7 +85,7 @@ class Alg_WC_PVBUR_Core {
 			( defined( 'DOING_AJAX' ) && DOING_AJAX ) ||
 			( current_filter() == 'pre_get_posts' && ! $query->is_single() && ! $query->is_search() ) ||
 			! $query->is_main_query() ||
-			empty( $query->query ) ||
+			// empty( $query->query ) ||
 			( isset( $query->query['post_type'] ) && $query->query['post_type'] == 'nav_menu_item' )
 		) {
 			return false;
