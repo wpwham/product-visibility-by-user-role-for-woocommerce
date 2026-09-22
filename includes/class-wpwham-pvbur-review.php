afadfab153362d876a0ae0c93e92d7b2a9cbd427
David Grant <support@wpwham.com>
2026-09-21 21:23:57 -0700

    rename new things to wpwham prefixes

--- includes/class-wpwham-pvbur-review.php
+++ includes/class-wpwham-pvbur-review.php
@@ -9,9 +9,9 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-if ( ! class_exists( 'Alg_WC_PVBUR_Review' ) ) :
+if ( ! class_exists( 'WPWham_PVBUR_Review' ) ) :
 
-class Alg_WC_PVBUR_Review {
+class WPWham_PVBUR_Review {
 
 	/**
 	 * Constructor.
@@ -40,26 +40,26 @@ class Alg_WC_PVBUR_Review {
 		}
         */
 
-		$dismissed = get_option( 'alg_wc_pvbur_review_dismissed' );
+		$dismissed = get_option( 'wpwham_pvbur_review_dismissed' );
 		if ( $dismissed === 'permanently' || ( is_numeric( $dismissed ) && $dismissed > time() ) ) {
 			return;
 		}
 
-		if ( isset( $_GET['alg_wc_pvbur_dismiss_review'] ) && isset( $_GET['_wpnonce'] ) ) {
-			if ( wp_verify_nonce( $_GET['_wpnonce'], 'alg_wc_pvbur_dismiss' ) ) {
+		if ( isset( $_GET['wpwham_pvbur_dismiss_review'] ) && isset( $_GET['_wpnonce'] ) ) {
+			if ( wp_verify_nonce( $_GET['_wpnonce'], 'wpwham_pvbur_dismiss' ) ) {
 				if ( isset( $_GET['later'] ) ) {
-					update_option( 'alg_wc_pvbur_review_dismissed', time() + ( DAY_IN_SECONDS * 30 ) );
+					update_option( 'wpwham_pvbur_review_dismissed', time() + ( DAY_IN_SECONDS * 30 ) );
 				} else {
-					update_option( 'alg_wc_pvbur_review_dismissed', 'permanently' );
+					update_option( 'wpwham_pvbur_review_dismissed', 'permanently' );
 				}
-				wp_redirect( remove_query_arg( array( 'alg_wc_pvbur_dismiss_review', '_wpnonce', 'later' ) ) );
+				wp_redirect( remove_query_arg( array( 'wpwham_pvbur_dismiss_review', '_wpnonce', 'later' ) ) );
 				exit;
 			}
 		}
 
-		$installed = get_option( 'alg_wc_pvbur_installed_time' );
+		$installed = get_option( 'wpwham_pvbur_installed_time' );
 		if ( ! $installed ) {
-			update_option( 'alg_wc_pvbur_installed_time', time() );
+			update_option( 'wpwham_pvbur_installed_time', time() );
 			return;
 		}
 
@@ -72,7 +72,7 @@ class Alg_WC_PVBUR_Review {
 		}
 
 		$review_url = 'https://wordpress.org/support/plugin/product-visibility-by-user-role-for-woocommerce/reviews/?rate=5#new-post';
-		$dismiss_url = wp_nonce_url( add_query_arg( 'alg_wc_pvbur_dismiss_review', '1' ), 'alg_wc_pvbur_dismiss' );
+		$dismiss_url = wp_nonce_url( add_query_arg( 'wpwham_pvbur_dismiss_review', '1' ), 'wpwham_pvbur_dismiss' );
 		$later_url = add_query_arg( 'later', '1', $dismiss_url );
 		?>
 		<div class="updated woocommerce-message">
@@ -108,4 +108,4 @@ class Alg_WC_PVBUR_Review {
 
 endif;
 
-return new Alg_WC_PVBUR_Review();
+return new WPWham_PVBUR_Review();
--- product-visibility-by-user-role-for-woocommerce.php
+++ product-visibility-by-user-role-for-woocommerce.php
@@ -275,7 +275,7 @@ final class Alg_WC_PVBUR {
 		$this->settings['bulk']    = require_once( 'includes/settings/class-alg-wc-pvbur-settings-bulk.php' );
 		add_action( 'woocommerce_system_status_report', array( $this, 'add_settings_to_status_report' ) );
 		// Review suggestion
-		require_once( 'includes/class-alg-wc-pvbur-review.php' );
+		require_once( 'includes/class-wpwham-pvbur-review.php' );
 		// Version updated
 		if ( get_option( 'alg_wc_pvbur_version', '' ) !== $this->version ) {
 			add_action( 'admin_init', array( $this, 'version_updated' ) );
